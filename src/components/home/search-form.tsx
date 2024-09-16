c5e6e0b0dcba1d68f166526c9f3e41d0a26588d9
Mollinea <mollinea99@gmail.com>
2024-09-16 01:50:45 -0400

    feat: search box now can be opened with keyboard

--- src/components/home/search-form.tsx
+++ src/components/home/search-form.tsx
@@ -9,7 +9,25 @@ export const SearchForm = () => {
   const [debouncedQuery, setDebouncedQuery] = useState("");
   const [query, setQuery] = useState("");
   const containerRef = useRef(null);
+  const inputRef = useRef<null | HTMLInputElement>(null);
   const boxRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleKeyComb = (e: KeyboardEvent) => {
+      if (e.altKey && e.key === "k" && inputRef.current) {
+        if (showResultsBox) {
+          inputRef.current.blur();
+          setShowResultsBox(false);
+        } else {
+          inputRef.current.focus();
+          setShowResultsBox(true);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyComb);
+    return () => window.removeEventListener("keydown", handleKeyComb);
+  }, [showResultsBox]);
 
   useEffect(() => {
     const handleTimeout = setTimeout(() => {
@@ -43,30 +61,42 @@ export const SearchForm = () => {
       className="relative border-r border-foreground/50 pr-3"
     >
       <form role="search" className="relative">
-        <div className="absolute right-0 flex h-full place-content-center items-center pr-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="text-accent/40"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-            <path d="M21 21l-6 -6" />
-          </svg>
+        <div className="absolute right-0 flex h-full place-content-center items-center pr-1">
+          <div className="md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-4 w-4 text-accent/40"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+              <path d="M21 21l-6 -6" />
+            </svg>
+          </div>
+          <div className="hidden items-center gap-0.5 text-[0.6rem] font-bold md:flex md:text-xs">
+            <kbd className="block rounded-md bg-card-background p-1 ring-1 ring-inset ring-accent/20">
+              alt
+            </kbd>
+            <span className="block">+</span>
+            <kbd className="block rounded-md bg-card-background p-1 ring-1 ring-inset ring-accent/20">
+              k
+            </kbd>
+          </div>
         </div>
         <input
+          ref={inputRef}
           role="searchbox"
           onChange={(e) => setDebouncedQuery(e.target.value)}
           onFocus={handleInputFocus}
           placeholder="Search components"
-          className="flex items-center rounded-md border-none bg-stone-900 pl-2 pr-10 ring-1 ring-accent/10 transition placeholder:text-accent/40 focus:outline-none focus:ring-primary max-md:max-w-[150px] max-md:text-sm max-md:placeholder:text-xs"
+          className="flex items-center rounded-md border-none bg-stone-900 pl-2 pr-10 ring-1 ring-accent/10 transition placeholder:text-accent/40 focus:outline-none focus:ring-primary max-md:max-w-[150px] max-md:text-sm max-md:placeholder:text-xs md:pr-16"
           type="text"
         />
       </form>
